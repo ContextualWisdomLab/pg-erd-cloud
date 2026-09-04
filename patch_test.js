49a2ecae8b9b10ed5fb90a7be1064a47f70fc861
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-09-04 12:55:57 +0000

    🎨 Palette: Fix broken tests after improving form accessibility

--- patch_test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-let testCode = fs.readFileSync('frontend/src/App.nativeFormSubmission.test.tsx', 'utf8');
-testCode = testCode.replace(
-  "await screen.findByRole('heading', { name: '대시보드' })\n\n    const projectName = screen.getByLabelText('New project')",
-  "await screen.findByRole('heading', { name: '대시보드' })\n\n    await user.click(screen.getByRole('button', { name: '편집기' }))\n\n    const projectName = await screen.findByLabelText('New project')"
-);
-
-fs.writeFileSync('frontend/src/App.nativeFormSubmission.test.tsx', testCode, 'utf8');
